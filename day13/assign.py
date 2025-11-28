de4488abd9f8250eee737881a528430fdc18a54e
increase <increaserejoice123@gmail.com>
2025-11-28 23:23:21 +0100

    Assignment done

---
+++ day13/assign.py
@@ -0,0 +1,20 @@
+class book:
+    print()
+    print("=====Book Recommendations")
+    print()
+
+    def __init__(self, Title, Author, Pages):
+        self.title = Title
+        self.author = Author
+        self.no_of_pages = Pages
+
+    def descripton(self):
+        print(f"Title: {self.title}\nAuthor: {self.author}\nPages: {self.no_of_pages}")
+
+CA = book("When a past came calling", "Chinua Achebe", 119)
+CA.descripton()
+
+print()
+
+MA = book("Psychology of money", "Morgan Housel", 105)
+MA.descripton()
